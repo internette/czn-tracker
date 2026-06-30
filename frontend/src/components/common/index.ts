48cc13e7bfb4a4c246a3e4f6ceca6ca675fe715d
Antoinette Janus <antoinettejanus@Antoinettes-Laptop.local>
2026-06-29 17:30:08 -0700

    Moved the location of Dropdon and TextInput to the correct UI folder

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -1,4 +1,2 @@
 export { default as CharacterCard } from './CharacterCard/CharacterCard'
 export { default as OwnedToggleBadge } from './OwnedToggleBadge/OwnedToggleBadge'
-export { default as Dropdown } from './Dropdown/Dropdown'
-export { default as TextInput } from './TextInput/TextInput'
